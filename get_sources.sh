213e636c121b261f5e608902a692319171b9fd71
llenotre <llenotre@student.42.fr>
2020-11-09 00:08:06 +0100

    Packages compilation WIP

--- get_sources.sh
+++ get_sources.sh
@@ -14,18 +14,28 @@ get_sources() {
 }
 
 extract_sources() {
+	mkdir -p builds
 	cd sources
 	ls -1 | while read file; do
 		echo "Extracting $file";
-		tar xvf "$file";
+		tar xvf "$file" -C ../builds >/dev/null 2>&1;
+		unzip "$file" -C ../builds >/dev/null 2>&1;
 	done
 	cd ..
 }
 
-#compile_sources() {
-	# TODO
-#}
+compile_sources() {
+	cd builds
+	ls -1 | while read file; do
+		echo "Compiling $file";
+		cd $file
+		./configure
+		make
+		cd ..
+	done
+	cd ..
+}
 
 get_sources
-#extract_sources
-#compile_sources()
+extract_sources
+compile_sources
