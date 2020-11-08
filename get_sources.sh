e3993afa9e5d51e2c6c2132276fc5fbd434757ee
llenotre <llenotre@student.42.fr>
2020-11-08 22:57:40 +0100

    Building script WIP

--- get_sources.sh
+++ get_sources.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
+get_sources() {
+	mkdir -p sources
+	cd sources
+	cat ../source_urls | while read file; do
+		output=${file##*/}
+		if ! stat $output >/dev/null 2>&1; then
+			echo "Downloading $file";
+			curl "$file" --output "$output";
+		fi
+	done
+	cd ..
+}
 
+extract_sources() {
+	cd sources
+	ls -1 | while read file; do
+		echo "Extracting $file";
+		tar xvf "$file";
+	done
+	cd ..
+}
+
+#compile_sources() {
+	# TODO
+#}
+
+get_sources
+#extract_sources
+#compile_sources()
