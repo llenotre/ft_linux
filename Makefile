36551fbb2fdd34a4ef69b7e8932079b12f604d94
llenotre <llenotre@student.42.fr>
2020-11-17 01:43:05 +0100

    Packages compilation WIP

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 KERNEL_VERSION = 4.19.157
-KERNEL_BIN = vmlinuz-$(KERNEL_VERSION)-llenotre
 KERNEL_SRC = kernel_src/
+KERNEL_BIN = $(KERNEL_SRC)/vmlinux
+KERNEL_BIN_NAME = vmlinuz-$(KERNEL_VERSION)-llenotre
+
+all: compile_packages $(KERNEL_BIN)
+	./compile_packages.sh
 
 $(KERNEL_BIN):
 	./download_kernel.sh
@@ -12,14 +16,13 @@ $(KERNEL_BIN):
 	make -C $(KERNEL_SRC)
 	cp kernel_src/vmlinux $(KERNEL_BIN)
 
-# TODO Compile packages
-
 tmp: tmp_linux.iso
 
-tmp_linux.iso: $(KERNEL) grub.cfg
+tmp_linux.iso: $(KERNEL_BIN) grub.cfg
 	mkdir -p iso/boot/grub/
 	cp grub.cfg iso/boot/grub/
-	cp $(KERNEL_BIN) iso/boot/
+	cp $(KERNEL_BIN) iso/boot/$(KERNEL_BIN_NAME)
+# TODO Copy packages
 	grub-mkrescue -o $@ iso
 
 clean:
@@ -28,7 +31,7 @@ clean:
 	rm -rf iso/
 
 fclean: clean
+	rm -rf pkg_sources
 	rm -f $(KERNEL_BIN)
-	# TODO Remove packages executables
 
-.PHONY: download_kernel tmp clean fclean
+.PHONY: compile_packages tmp clean fclean
--- compile_packages.sh
+++ compile_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 get_sources() {
-	mkdir -p sources
-	cd sources
+	mkdir -p pkg_sources
+	cd pkg_sources
 	cat ../source_urls | while read file; do
 		output=${file##*/}
 		if ! stat $output >/dev/null 2>&1; then
@@ -14,22 +14,25 @@ get_sources() {
 }
 
 extract_sources() {
-	mkdir -p builds
-	cd sources
-	ls -1 | while read file; do
-		echo "Extracting $file";
-		tar xvf "$file" -C ../builds >/dev/null 2>&1;
-		unzip "$file" -C ../builds >/dev/null 2>&1;
-	done
-	cd ..
+	if ! stat pkg_builds >/dev/null 2>&1; then
+		mkdir -p pkg_builds
+		cd pkg_sources
+		ls -1 | while read file; do
+			echo "Extracting $file";
+			tar xvf "$file" -C ../pkg_builds >/dev/null 2>&1;
+			unzip "$file" -C ../pkg_builds >/dev/null 2>&1;
+		done
+		cd ..
+	fi
 }
 
 compile_sources() {
-	cd builds
+	mkdir -p pkg_out
+	cd pkg_builds
 	ls -1 | while read file; do
 		echo "Compiling $file";
 		cd $file
-		./configure
+		./configure --prefix ../pkg_out
 		make
 		cd ..
 	done
