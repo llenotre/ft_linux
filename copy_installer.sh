10a82d7e9a006e28f73a852e9396f96406914791
llenotre <llenotre@student.42.fr>
2020-12-10 16:22:13 +0100

    Fixing temporary system

--- copy_installer.sh
+++ copy_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p iso/install
-cp pkg_tarballs iso/install
-cp scripts iso/install
+cp -r pkg_tarballs iso/install
+cp -r scripts iso/install
 cp compile_packages.sh iso/install
 cp source_urls iso/install
 cp deps iso/install
